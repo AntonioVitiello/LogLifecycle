b3237ebaa43f98f4d73334e33498d1ea5cfa097a
Vitiello Antonio <vitiello.antonio@gmail.com>
2017-12-04 23:37:47 +0100

    Logger debug

--- app/src/main/java/com/util/Log.java
+++ app/src/main/java/com/util/Log.java
@@ -10,6 +10,14 @@ public class Log {
     private static boolean sIsLogTagConcat = false;
     private static boolean sDebug = true;
 
+    public static void init(boolean debug) {
+        init(debug, null, false);
+    }
+
+    public static void init(boolean debug, String logTag) {
+        init(debug, logTag, true);
+    }
+
     public static void init(boolean debug, String logTag, boolean isLogTagConcat) {
         sDebug = debug;
         sIsLogTagDefined = logTag != null;
